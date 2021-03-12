205613e0fdd022864bcf1631cf7476d3abc69e49
Romulo Martins Pereira <mp.romulo@gmail.com>
2021-03-12 13:41:32 -0300

    Inserir categoria (serviço e recurso)/ POST ok

--- src/main/java/com/monografia/forum/controllers/CategoriaController.java
+++ src/main/java/com/monografia/forum/controllers/CategoriaController.java
@@ -1,34 +1,48 @@
 package com.monografia.forum.controllers;
 
+import java.net.URI;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.monografia.forum.dto.CategoriaDto;
 import com.monografia.forum.services.CategoriaService;
 
 @RestController
-@RequestMapping(value="/categorias")
+@RequestMapping(value = "/categorias")
 public class CategoriaController {
-	
+
 	@Autowired
 	private CategoriaService service;
-	
+
 	@GetMapping
-	public ResponseEntity<List<CategoriaDto>> findAll(){
+	public ResponseEntity<List<CategoriaDto>> findAll() {
 		List<CategoriaDto> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
-	
+
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<CategoriaDto> findById(@PathVariable Long id){
+	public ResponseEntity<CategoriaDto> findById(@PathVariable Long id) {
 		CategoriaDto dto = service.findById(id);
 		return ResponseEntity.ok().body(dto);
+	}
+
+	@PostMapping
+	public ResponseEntity<CategoriaDto> insert(@Valid @RequestBody CategoriaDto dto) {
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
 	}
 
 }
--- src/main/java/com/monografia/forum/services/CategoriaService.java
+++ src/main/java/com/monografia/forum/services/CategoriaService.java
@@ -9,14 +9,26 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.monografia.forum.dto.CategoriaDto;
+import com.monografia.forum.dto.SubcategoriaDto;
+import com.monografia.forum.dto.TopicoDto;
 import com.monografia.forum.entities.Categoria;
+import com.monografia.forum.entities.Subcategoria;
+import com.monografia.forum.entities.Topico;
 import com.monografia.forum.repositories.CategoriaRepository;
+import com.monografia.forum.repositories.SubcategoriaRepository;
+import com.monografia.forum.repositories.TopicoRepository;
 
 @Service
 public class CategoriaService {
 	
 	@Autowired
 	private CategoriaRepository repository;
+	
+	@Autowired
+	private SubcategoriaRepository subcategoriaRepository;
+	
+	@Autowired
+	private TopicoRepository topicoRepository;
 	
 	@Transactional(readOnly = true)
 	public List<CategoriaDto> findAll(){
@@ -35,4 +47,27 @@ public class CategoriaService {
 		return (new CategoriaDto(categoria, categoria.getTopicos(), categoria.getSubcategorias()));
 	}
 
+	@Transactional
+	public CategoriaDto insert(CategoriaDto dto) {
+		Categoria entity = new Categoria();
+		copyDtoToEntity(dto, entity);
+		entity = repository.save(entity);
+		return new CategoriaDto(entity);
+	}
+
+	private void copyDtoToEntity(CategoriaDto dto, Categoria entity) {
+		entity.setNome(dto.getNome());
+		
+		entity.getSubcategorias().clear();
+		for (SubcategoriaDto subcategoriaDto : dto.getSubcategorias()) {
+			Subcategoria subcategoria = subcategoriaRepository.getOne(subcategoriaDto.getId());
+			entity.getSubcategorias().add(subcategoria);
+		}
+		
+		entity.getTopicos().clear();
+		for (TopicoDto topicoDto : dto.getTopicos()) {
+			Topico topico = topicoRepository.getOne(topicoDto.getId());
+			entity.getTopicos().add(topico);
+		}
+	}
 }
